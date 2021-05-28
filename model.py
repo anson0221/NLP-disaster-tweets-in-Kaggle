88978d9b5e3a854ddfa7267a25d95da03f3cf793
Anson_Wei <anson_wei@hutingweide-MacBook-Pro.local>
2021-05-29 00:07:40 +0800

    Finish the file.

--- model.py
+++ model.py
@@ -7,18 +7,15 @@ import torch.nn.functional as F
 
 
 class sentencesVec(nn.Module):
-    def __init__(self, bert: str, layer_index: int=-2):
-        self.bert_tweet = AutoModel.from_pretrained("vinai/bertweet-base")
+    def __init__(self, bert: str="vinai/bertweet-base", layer_index: int=-2):
+        self.bert_tweet = AutoModel.from_pretrained(bert)
         self.idx = layer_index
 
-    def forward(self, kw, sent):
+    def forward(self, sent):
         """
         transform sentences to vector by BERT
-            * keyword: (batch_size)
-            * sentence: (batch_size, seq_len)
+            * sent: (batch_size, seq_len)
         """
-        self.idx = -2 # get the second-to-last layer output of BERT
-
 
         # object : tuple = (the output of the embeddings, the output of each layer)
         object = self.bert_tweet(sent)
@@ -28,13 +25,12 @@ class sentencesVec(nn.Module):
         return hd_states # (batch, seq_len, hidden_dim)
 
 class Classifier_bert(nn.Module):
-    def __init__(self, kw_embd_dim: int=10, src_bert: str="vinai/bertweet-base", attn_num :int=2, out_dim: int=2):
-        self.kw_embedding_dim = kw_embd_dim
+    def __init__(self, src_bert: str="vinai/bertweet-base", bert_layerChoice: int=-2, attn_num :int=2, out_dim: int=2):
         self.sourceBert = src_bert
         self.attentionNum = attn_num
         self.outNum = out_dim
     
-        self.cvtr_layer = sentencesVec(self.sourceBert)
+        self.cvtr_layer = sentencesVec(bert=self.sourceBert, layer_index=bert_layerChoice)
 
         self.tweet_extractor = nn.Sequential(
             nn.Linear(768, 384),
@@ -62,15 +58,15 @@ class Classifier_bert(nn.Module):
         self.out = nn.Linear(1, self.outNum)
         self.logSoftmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, text, keyword, kw_len):
+    def forward(self, kw_len, text):
         """
-        text: (batch_size)
-        keyword: (batch_size)
+        kw_len: (batch_size)
+        text: (batch_size, seq_len)
         """
 
         # get contextualized embedding
         batch_size = text.shape[0]
-        sentVec = self.cvtr_layer(keyword + text) # sentVec: (batch_size, total_seq_len, 768)
+        sentVec = self.cvtr_layer(text) # sentVec: (batch_size, total_seq_len, 768)
 
         tweet_vec = sentVec[:, kw_len:, :] # tweet_vec: (batch_size, tweet_seq_len, 768)
         tweet_vec = self.tweet_extractor(tweet_vec) # tweet_vec: (batch_size, tweet_seq_len, 16)
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from torch import optim
 from dataset import DisasTweet_ds, collater
+from model import Classifier_bert
 from tqdm import tqdm
 import os
 import sys
@@ -12,9 +13,12 @@ import sys
 def train(
             from_ck_point :bool,
             model_path :str,
-            optimizer__ :str='SGD',
+            data_path :str='./data/trasin.csv',
             criterion=nn.NLLLoss(),
-            source_bert="vinai/bertweet-base",
+            source_bert='vinai/bertweet-base',
+            choose_bert_layer_as_embd: int=-2,
+            attentionNUM: int=2,
+            tgt_categoryNum: int=2, 
             batch_size_=64, 
             epochs=20,
             clip=1,
@@ -22,6 +26,88 @@ def train(
             device='cpu'
 ):
     # dataset
-    dataset_ = DisasTweet_ds(src_bert=source_bert)
+    dataset_ = DisasTweet_ds(train_file_path=data_path, src_bert=source_bert)
     collate_fn_ = collater(pad_value=dataset_.pad_token)
     train_loader = DataLoader(dataset=dataset_, batch_size=batch_size_, collate_fn=collate_fn_, shuffle=True, drop_last=True)
+
+    # model
+    model = Classifier_bert(
+        src_bert=source_bert, 
+        bert_layerChoice=choose_bert_layer_as_embd, 
+        attn_num=attentionNUM, 
+        out_dim=tgt_categoryNum
+    )
+
+    if from_ck_point:
+        check_point = torch.load(model_path, map_location=device)
+        model.load_state_dict(check_point['model_state_dict'])
+        BEST_LOSS = check_point['loss']
+
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    model.train()
+
+    # training
+    BEST_LOSS = 999999
+    for epoch in range(epochs):
+        epoch_loss = 0
+        print()
+        print('Epoch #'+str(epoch))
+        for len_kw, sentence, target in tqdm(train_loader):
+            """
+            * len_kw: (batch_size)
+            * sentence: (batch_size, seq_len)
+            * target: (batch_size)
+            """
+
+            # out: (batch_size, 2)
+            out = model(kw_len=len_kw, text=sentence)
+
+            loss = 0
+            optimizer.zero_grad()
+            for i in range(batch_size_):
+                loss += criterion(out, target)
+
+            loss.backward()
+            clip_grad_norm_(parameters=model.parameters(), max_norm=clip) # clipping gradient
+            optimizer.step()
+
+            epoch_loss += loss.item()
+        
+        epoch_loss /= len(train_loader)
+        print('Loss : '+str(epoch_loss))
+
+        if epoch_loss<BEST_LOSS:
+            # save the model
+            BEST_LOSS = epoch_loss
+            torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': epoch_loss,
+            }, model_path)
+
+
+if __name__=='__main__':
+    from_check_point = False
+    modelPath = './experiment/'+sys.argv[1]
+    dataPath = './data/train.csv'
+    srcBERT = 'vinai/bertweet-base'
+    bertlayer_idx = -2
+    attn_layerNum = 2
+    batchSize = 16
+    epoch_num = 30
+    lr = 0.007
+    device_ = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    train(
+        from_ck_point=from_check_point,
+        model_path=modelPath,
+        data_path=dataPath,
+        source_bert=srcBERT,
+        choose_bert_layer_as_embd=bertlayer_idx,
+        attentionNUM= attn_layerNum,
+        batch_size_=batchSize,
+        epochs=epoch_num,
+        learning_rate=lr,
+        device=device_
+    )
