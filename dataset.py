09a657b4709d07606d62bc7d97027377863e76be
Anson_Wei <anson_wei@hutingweide-MacBook-Pro.local>
2021-05-29 00:03:56 +0800

    Remove URL.

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from torch.utils.data.dataset import Dataset
+import re
 import torch
 from transformers import AutoTokenizer
 from torch.nn.utils.rnn import pad_sequence
@@ -12,33 +13,29 @@ class collater():
         """
         sample (a list for a batch of samples) : 
             [
-                (keyword_0: str, len_of_keyword_0, sentence_0: list[int], target_0: int), 
-                (keyword_1: str, len_of_keyword_1, sentence_1: list[int], target_1: int),
+                (len_of_keyword_0: int, sentence_0: list[int], target_0: int), 
+                (len_of_keyword_1: int, sentence_1: list[int], target_1: int),
                 ..., 
-                (keyword_n: str, len_of_keyword_n, sentence_n: list[int], target_n: int),
+                (len_of_keyword_n: int, sentence_n: list[int], target_n: int),
             ]
 
         return values:
-            * keyword: (batch_size)
+            * len_kw: (batch_size)
             * sentence: (batch_size, seq_len)
             * target: (batch_size)
         """
 
-        keyword = []
         len_kw = []
         sentence = []
         target = []
-        for kw, len_of_kw, sent, tgt in sample:
-            keyword.append(kw)
+        for len_of_kw, sent, tgt in sample:
             len_kw.append(len_of_kw)
             sentence.append(sent)
             target.append(tgt)
 
-        keyword = pad_sequence(keyword, batch_first=True, padding_value=self.padding)
         sentence = pad_sequence(sentence, batch_first=True, padding_value=self.padding)
-        target = pad_sequence(sentence, batch_first=True)
 
-        return keyword, len_kw, sentence, target
+        return len_kw, sentence, target
 
 
 class DisasTweet_ds(Dataset):
@@ -49,16 +46,23 @@ class DisasTweet_ds(Dataset):
         self.text = df['text'].fillna('none text.')
         self.target = df['target']
 
+        self._preprocess_text()
+
         self.tknzr_tweet = AutoTokenizer.from_pretrained(src_bert, use_fast=False)
         self.pad_token = self.tknzr_tweet.get_vocab()[self.tknzr_tweet.pad_token]
+    
+    def _preprocess_text(self):
+        html = re.compile(r'((http)|(https))://\S+') # \S: all non-space symbols
+        for sent in range(self.text):
+            html.sub(repl=r'', string=sent)
 
     def __getitem__(self, idx):
-        kw = torch.tensor(self.tknzr_tweet.encode(self.keyword[idx]+':'))
+        kw = torch.tensor(self.tknzr_tweet.encode('['+self.keyword[idx]+']:'))
         kw_len = kw.size()[0]
         sent = torch.tensor(self.tknzr_tweet.encode(self.text[idx]))
         tgt = torch.tensor(self.target[idx])
 
-        return kw, kw_len, sent, tgt
+        return kw_len, torch.cat((kw, sent), dim=0), tgt
 
     def __len__(self):
         return len(self.text)
